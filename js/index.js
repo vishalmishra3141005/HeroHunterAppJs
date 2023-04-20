f0f2e1542504efb931bb4dd6ef135760f62660a4
Vishal Mishra <vishalmishra3141005@gmail.com>
2023-04-20 22:39:55 +0530

    added get character fetchapi call

--- js/index.js
+++ js/index.js
@@ -5,8 +5,25 @@ api and create the home page */
 import { auth } from "./module/AuthenticationData.js";
 
 
-function homeLoader() {
-    console.log(auth);
+
+async function homeLoader() {
+    const baseUrl = "https://gateway.marvel.com";
+    let getCharacterApi = "/v1/public/characters";
+
+    let getApi = `${baseUrl}/${getCharacterApi}?${auth}`;
+    let jsonData;
+    
+    try {
+        let response = await fetch(getApi);
+        try {
+            jsonData = await response.json();
+        } catch(e) {
+            console.log(`unable to parse json - ${e}`);
+        }
+    } catch(e) {
+        console.log(`Unable to fetch data - ${e}`);
+    }
+    console.log(jsonData);
 }
 
 homeLoader();
