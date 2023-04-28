28554677e278328f2fa9cfd4680649737092f8fd
Vishal Mishra <vishalmishra3141005@gmail.com>
2023-04-28 20:39:23 +0530

    added comics, series, stories and events in handlerjs

--- js/HeroHandler.js
+++ js/HeroHandler.js
@@ -19,6 +19,53 @@ let seriesVis = false;
 let storiesVis = false;
 let eventsVis = false;
 
+const comics = document.getElementById("comics");
+const series = document.getElementById("series");
+const stories = document.getElementById("stories");
+const events = document.getElementById("events");
+
+const comicsHeading = document.getElementById("comics-heading");
+comicsHeading.addEventListener("click", function(e) {
+    if (comicsVis) {
+        comics.style.display = "none";
+    } else {
+        comics.style.display = "block";
+    }
+    comicsVis = !comicsVis
+});
+
+
+const seriesHeading = document.getElementById("series-heading");
+seriesHeading.addEventListener("click", function(e) {
+    if (seriesVis) {
+        series.style.display = "none";
+    } else {
+        series.style.display = "block";
+    }
+    seriesVis = !seriesVis;
+});
+
+
+const storiesHeading = document.getElementById("stories-heading");
+storiesHeading.addEventListener("click", function(e) {
+    if (storiesVis) {
+        stories.style.display = "none";
+    } else {
+        stories.style.display = "block";
+    }
+    storiesVis = !storiesVis;
+});
+
+const eventsHeading = document.getElementById("events-heading");
+eventsHeading.addEventListener("click", function(e) {
+    if (eventsVis) {
+        events.style.display = "none";
+    } else {
+        events.style.display = "block";
+    }
+    eventsVis = !eventsVis;
+});
+
 
 async function fetchData() {
     try {
@@ -53,55 +100,53 @@ function loadCharacter(apiData) {
     const aboutContainer = document.querySelector(".about-container");
     aboutContainer.innerText = `${mainData.description}`
 
+    const comicContainer = comics.firstElementChild;
 
+    const comicData = mainData.comics.items;
 
-    const comics = document.getElementById("comics");
-    const series = document.getElementById("series");
-    const stories = document.getElementById("stories");
-    const events = document.getElementById("events");
+    const comicArray = comicData.map((currComic) => {
+        return `<li class="item-style">${currComic.name}</li>`;
+    });
+    comicContainer.innerHTML = "";
+    for (let currComics of comicArray) {
+        comicContainer.innerHTML += currComics;
+    }
+    
+    const seriesContainer = series.firstElementChild;
 
-    const comicsHeading = document.getElementById("comics-heading");
-    comicsHeading.addEventListener("click", function(e) {
-        if (comicsVis) {
-            comics.style.display = "none";
-        } else {
-            comics.style.display = "block";
-        }
-        comicsVis = !comicsVis
+    const seriesData = mainData.series.items;
+    const seriesArray = seriesData.map((currSeries) => {
+        return `<li class="item-style">${currSeries.name}</li>`;
     });
 
+    seriesContainer.innerHTML = "";
+    for (let currSeries of seriesArray) {
+        seriesContainer.innerHTML += currSeries;
+    }
 
-    const seriesHeading = document.getElementById("series-heading");
-    seriesHeading.addEventListener("click", function(e) {
-        if (seriesVis) {
-            series.style.display = "none";
-        } else {
-            series.style.display = "block";
-        }
-        seriesVis = !seriesVis;
+    const storiesContainer = stories.firstElementChild;
+
+    const storiesData = mainData.stories.items;
+    const storiesArray = storiesData.map((currStories) => {
+        return `<li class="item-style">${currStories.name}</li>`;
     });
 
+    storiesContainer.innerHTML = "";
+    for (let currStories of storiesArray) {
+        storiesContainer.innerHTML += currStories;
+    }
 
-    const storiesHeading = document.getElementById("stories-heading");
-    storiesHeading.addEventListener("click", function(e) {
-        if (storiesVis) {
-            stories.style.display = "none";
-        } else {
-            stories.style.display = "block";
-        }
-        storiesVis = !storiesVis;
+    const eventsContainer = events.firstElementChild;
+
+    const eventsData = mainData.events.items;
+    const eventsArray = eventsData.map((currEvents) => {
+        return `<li class="item-style">${currEvents.name}</li>`
     });
 
-    const eventsHeading = document.getElementById("events-heading");
-    eventsHeading.addEventListener("click", function(e) {
-        if (eventsVis) {
-            events.style.display = "none";
-        } else {
-            events.style.display = "block";
-        }
-        eventsVis = !eventsVis;
-    });
-
+    eventsContainer.innerHTML = "";
+    for (let currEvents of eventsArray) {
+        eventsContainer.innerHTML += currEvents;
+    }
 }
 
 function displayLoader() {
