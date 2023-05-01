9e7b86336bfd04e7c0ce39fc6395fee311e737e0
Vishal Mishra <vishalmishra3141005@gmail.com>
2023-05-01 19:56:32 +0530

    added favicon handler and firebase setup

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ import { auth } from "./module/AuthenticationData.js";
 import { characterData, next, prev } from "./module/PageElement.js";
 import { displayLoader, hideLoader } from "./module/LoaderHandler.js";
 import { searchBar, searchButton } from "./module/SearchBarHandler.js";
-
+import { addCharacter, removeCharacter, getCharacter } from "./module/FireBaseAuth.js";
 
 let offset =  0;
 // let pageNumber = 0;
@@ -15,6 +15,7 @@ const limit = 15;
 let currentCount = 0;
 let searchTerm = ""
 let searchOffset = 0;
+
 
 async function fetchCharacters() {
     const baseUrl = "https://gateway.marvel.com";
@@ -43,15 +44,13 @@ async function fetchCharacters() {
     }
 }
 
-
-
-function loadAllCharacters(jsonData) {
+async function loadAllCharacters(jsonData) {
     let cell = 0;
     let results = jsonData.data.results;
     
     let parent;
     currentCount = results.length;
-    console.log(results);
+
     if (currentCount === 0) {
         alert("No data!");
         window.open("/index.html", "_self");
@@ -73,17 +72,77 @@ function loadAllCharacters(jsonData) {
         newChild.setAttribute("id", results[idx].id);
         newChild.setAttribute("class", "characters");
         newChild.addEventListener("click", function(e) {
-            window.open(`/hero.html?characterID=${results[idx].id}`, "_self");
+            window.open(`/hero.html?characterID=${results[idx].id}`, "_blank");
         });
+
         let heroImage = results[idx].thumbnail;
 
-        newChild.innerHTML = `
-        <img class="img-style" src="${heroImage.path}.${heroImage.extension}" alt="HeroLogo">
-        <div class="about-hero">
-            <div class="hero-name">${results[idx].name}</div>
-            <img class="fav-icon" src="/assets/tile002.png">
-        </div>`;
-        // console.log(newChild);
+        let favData = null;
+        try {
+            favData = await getCharacter(results[idx].id);
+        } catch (err) {
+            favData = null;
+        }
+
+        let favIcon = `${favData ? "/assets/tile001.png" : "/assets/tile002.png"}`;
+
+
+        // newChild.innerHTML = `
+        // <img class="img-style" src="${ heroImage.path }.${ heroImage.extension }" alt="HeroLogo">
+        // <div class="about-hero">
+        //     <div class="hero-name">${ results[idx].name }</div>
+        //     <img class="fav-icon" src="${ favIcon }" onClick="${favIconHandler()}" >
+        // </div>`;
+
+
+        const childImg = document.createElement("img");
+        childImg.setAttribute("class", "img-style");
+        childImg.setAttribute("src", `${ heroImage.path }.${ heroImage.extension }`);
+        childImg.setAttribute("alt", "HeroLogo");
+
+        newChild.appendChild(childImg);
+        const aboutHero = document.createElement("div");
+        aboutHero.setAttribute("class", "about-hero");
+
+        const heroName = document.createElement("div");
+        heroName.setAttribute("class", "hero-name");
+        heroName.innerText = `${ results[idx].name }`;
+
+        const heroFavIcon = document.createElement("img");
+        heroFavIcon.setAttribute("class", "fav-icon");
+        heroFavIcon.setAttribute("src", `${ favIcon }`);
+        heroFavIcon.addEventListener("click", async function(e) {
+            e.stopPropagation();
+            let newFavData = null;
+            try {
+                newFavData = await getCharacter(results[idx].id);
+            } catch (err) {
+                newFavData = null;
+            }
+            const charId = `${ results[idx].id }`;
+            const newData = { 
+                id: `${results[idx].id}`,
+                img: `${ heroImage.path }.${ heroImage.extension }`,
+                name: `${ results[idx].name }`,
+            };
+            let doesItExist = newFavData !== null ? true : false;
+            if (doesItExist) {
+                removeCharacter(charId);
+                doesItExist = false;              
+            } else {
+                addCharacter(charId, newData);
+                doesItExist = true;
+            }
+            console.log(await getCharacter(charId));
+            favIcon = `${doesItExist ? "/assets/tile001.png" : "/assets/tile002.png"}`;
+            heroFavIcon.setAttribute("src", `${ favIcon }`);
+        });
+
+        aboutHero.appendChild(heroName);
+        aboutHero.appendChild(heroFavIcon);
+        newChild.append(aboutHero);
+
+
         parent.appendChild(newChild);
         cell++;
         if (cell == 5 || idx === results.length - 1) {
@@ -133,6 +192,10 @@ searchButton.addEventListener("click", function(e) {
     fetchCharacters();
 });
 
+
+
+offset = 0;
+searchOffset = 0;
 searchBar.value = "";
 displayLoader();
 fetchCharacters();
---
+++ js/module/FireBaseAuth.js
@@ -0,0 +1,59 @@
+
+
+  // Import the functions you need from the SDKs you need
+
+import * as firebaseApp from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import * as firebaseDatabase from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js"
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyC-UxCx4bRKK3MXRlBXgmGzz5H6kxybs9E",
+
+    authDomain: "herohunter-27d7b.firebaseapp.com",
+
+    databaseURL: "https://herohunter-27d7b-default-rtdb.firebaseio.com",
+
+    projectId: "herohunter-27d7b",
+
+    storageBucket: "herohunter-27d7b.appspot.com",
+
+    messagingSenderId: "644428061813",
+
+    appId: "1:644428061813:web:a2032fd0cf4584df73ec3c"
+
+};
+
+
+  // Initialize Firebase
+
+const app = firebaseApp.initializeApp(firebaseConfig);
+
+const db = firebaseDatabase.getDatabase();
+
+export function addCharacter(charId, characterData) {
+    const charRef = firebaseDatabase.ref(db, `characters/${charId}`);
+    firebaseDatabase.set(charRef, characterData);
+}
+
+export async function removeCharacter(charId) {
+    const charRef = firebaseDatabase.ref(db, `characters/${charId}`);
+    await firebaseDatabase.remove(charRef);
+}
+
+export async function getCharacter(charId) {
+    const dbRef = firebaseDatabase.ref(db);
+    try {
+        let snapshot = await firebaseDatabase.get(firebaseDatabase.child(dbRef, `characters/${charId}`));
+        return snapshot.val();
+    } catch(err) {
+        console.log(err);
+        return null;
+    }
+}
